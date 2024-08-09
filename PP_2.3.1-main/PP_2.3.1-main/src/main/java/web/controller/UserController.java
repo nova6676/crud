0202757674193b209e84706d18f2a553aa3d37e9
nova6676 <134915434+nova6676@users.noreply.github.com>
2024-08-09 10:59:03 +0400

    Add files via upload

---
+++ PP_2.3.1-main/PP_2.3.1-main/src/main/java/web/controller/UserController.java
@@ -0,0 +1,72 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.service.UserService;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/")
+public class UserController {
+
+    @Autowired
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String users(Model model) {
+        model.addAttribute("showAllUsers", userService.getAllUsers());
+        return "showAllUsers";
+    }
+
+    @GetMapping("/{id}")
+    public String getUserById(@PathVariable("id") long id, Model model) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "user";
+    }
+
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("user") User user) {
+        return "new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "/new";
+        }
+        userService.save(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") long id) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("user") @Valid User user,
+                         BindingResult bindingResult, @PathVariable("id") long id) {
+        if (bindingResult.hasErrors()){
+            return "edit";
+        }
+        userService.update(user);
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") long id) {
+        userService.removeUserById(id);
+        return "redirect:/";
+    }
+
+}
